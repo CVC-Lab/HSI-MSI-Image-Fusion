07ae8ddcb17047dabd2746d394a0e3eb19c2b1ef
shubham0704 <sb42451112@gmail.com>
2022-09-14 12:49:16 +0530

    minor changes

--- hmi_fusion/datasets/indian_pines.py
+++ hmi_fusion/datasets/indian_pines.py
@@ -1,2 +1,17 @@
-from datasets import load_dataset
-dataset = load_dataset("cvc-lab/IndianPines")
+from torch.utils.data import Dataset
+# from datasets import load_dataset
+# dataset = load_dataset("cvc-lab/IndianPines")
+
+# download an unzip dataset from huggingface
+
+# need to write local dataset dataloader
+
+class IndianPinesDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
