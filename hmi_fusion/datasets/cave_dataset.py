ff2f60182540d2a7a68c14898a0ebd5cb8b20d26
shubham0704 <sb42451112@gmail.com>
2022-09-21 05:46:35 +0000

    minor changes

--- hmi_fusion/datasets/cave_dataset.py
+++ hmi_fusion/datasets/cave_dataset.py
@@ -12,59 +12,6 @@ def load_ms_img(ms_path):
         ms_img.append(np.array(im))
     return np.moveaxis(np.array(ms_img), 0, -1)
 
-<<<<<<< HEAD
-# def create_hsi():
-    # Shifted blur kernel
-    k0 = matlab_style_gauss2D()
-    radius = 6
-    k = np.zeros((13,13))
-    k[0:9,0:9] = k0
-    
-    center = np.zeros((2,1))
-    center[0]=N1/2+1
-    center[1]=N2/2+1
-    
-    I = np.linspace(center[0]-radius,center[1]+radius,13)
-    I2 = np.linspace(center[0]-radius,center[1]+radius,13)
-    for _ in range(12):
-        I2 = np.append(I2,I)
-    
-    I2 = np.reshape(I2,(169,1))
-    J = np.linspace(center[1]-radius,center[1]+radius,13)
-    J = np.repeat(J,13,axis = 0)
-    
-    arr = np.array([I2,J])
-    arr = np.reshape(arr,(2,169))
-    arr = arr.astype(int)
-    I2 = I2.astype(int)
-    J = J.astype(int)
-    inds = np.ravel_multi_index(arr,(N1-1,N2-1))
-    a=0
-    kk = np.zeros((N1,N2))
-    for i in range(13):
-        for j in range(13):
-            
-            kk[I2[a],J[a]] = k[j,i]
-            a=a+1
-        
-    #Shift Kernel
-    kk = np.roll(kk,-int((N1+2)/2), axis=0)
-    kk = np.roll(kk,-int((N2+2)/2), axis=1)
-    fft = np.fft.fft2(kk)
-    
-    hsi = np.zeros((n1,n2,N3))
-    
-    for band in range(N3):
-        x = sri[:,:,band]
-        Fx = np.fft.fft2(x)
-        x = np.multiply(Fx,fft)
-        x = np.real(np.fft.ifft2(x))
-        hsi[:,:,band] = x[1:-1:4,1:-1:4]
-    
-    
-    hsi,noise_ten2 = add_noise(hsi,30)
-=======
->>>>>>> 8abc09d12f7737582bde0d8b1e8111941617a088
 
 test_classes = ["balloons_ms", "cd_ms", "cloth_ms", "photo_and_face_ms", "thread_spools_ms"]
 
@@ -80,13 +27,8 @@ def load_data(dataset_dir, mode):
         classes = [c for c in classes if c not in test_classes]
     
     for c in os.listdir(dataset_dir):
-<<<<<<< HEAD
         if c not in classes: continue
         
-=======
-        if mode == "test":
-            if c not in test_classes: continue
->>>>>>> 8abc09d12f7737582bde0d8b1e8111941617a088
         c_path = os.path.join(dataset_dir, c, c)
         if not os.path.isdir(c_path): continue
         ms_path = []
