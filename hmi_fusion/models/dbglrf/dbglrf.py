80efce6bf5a04df6d3aaa9ede32a9e7d941e9394
shubham0704 <sb42451112@gmail.com>
2022-11-08 02:55:41 +0000

    final dl model

--- hmi_fusion/models/dbglrf/dbglrf.py
+++ hmi_fusion/models/dbglrf/dbglrf.py
@@ -21,6 +21,13 @@ from ..hip.fusion.getLaplacian import getLaplacian
 import scipy
 dtype = torch.FloatTensor
 
+def calc_sam_loss(pred, tgt):
+    return torch.arccos((pred * tgt)/(torch.linalg.norm(pred)*torch.linalg.norm(tgt))).mean()
+
+
+
+
+
 class Decoder(nn.Module):
     def __init__(self, dec_channels):
         super(Decoder, self).__init__()
@@ -92,16 +99,9 @@ class Encoder(nn.Module):
 class AutoEncoder(nn.Module):
     def __init__(self, in_channels, dec_channels):
         super(AutoEncoder, self).__init__()
-        
         self.in_channels = in_channels
         self.dec_channels = dec_channels
-        ###############
-        # ENCODER
-        ##############
         self.encoder = Encoder(in_channels)
-        ###############
-        # DECODER
-        ##############
         self.decoder = Decoder(dec_channels)
         # Reinitialize weights using He initialization
         for m in self.modules():
@@ -124,27 +124,97 @@ class AutoEncoder(nn.Module):
             x_hat = self.decoder(y)
             return x_hat
 
+    def calc_loss(self, x_hat, y_hat, lz, x, y):
+        loss = nn.MSELoss()
+        if isinstance(y_hat, None):
+            recon_loss_y = None
+        else:
+            recon_loss_y = loss(y_hat, y)
+        recon_loss_x = loss(x_hat, x)
+        # SAM loss
+        sam_loss = calc_sam_loss(x_hat, x)
+        ### Graph Laplacian Loss
+        x_hat = x_hat.reshape(x_hat.shape[0], x_hat.shape[1], -1)
+        lz_xt = linear_operator.utils.sparse.bdsmm(lz, x_hat.transpose(1,2))
+        GL = MatmulLinearOperator(x_hat, lz_xt)
+        factor = x_hat.shape[-1]
+        GL = torch.diagonal(to_dense(GL)/factor, dim1=-2, dim2=-1).sum() # trace
+        return recon_loss_x, sam_loss, recon_loss_y, GL
 
-def calc_sam_loss(pred, tgt):
-    return torch.arccos((pred * tgt)/(torch.linalg.norm(pred)*torch.linalg.norm(tgt))).mean()
 
-def calc_loss(x_hat, y_hat, lz, x, y):
-    loss = nn.MSELoss()
-    if isinstance(y_hat, None):
-        recon_loss_y = None
-    else:
-        recon_loss_y = loss(y_hat, y)
-    recon_loss_x = loss(x_hat, x)
-    # SAM loss
-    sam_loss = calc_sam_loss(x_hat, x)
-    ### Graph Laplacian Loss
-    # x_hat = x_hat * transform.std[None, :, None, None] + transform.mean[None, :, None, None]
-    x_hat = x_hat.reshape(x_hat.shape[0], x_hat.shape[1], -1)
-    lz_xt = linear_operator.utils.sparse.bdsmm(lz, x_hat.transpose(1,2))
-    GL = MatmulLinearOperator(x_hat, lz_xt)
-    factor = x_hat.shape[-1]
-    GL = torch.diagonal(to_dense(GL)/factor, dim1=-2, dim2=-1).sum() # trace
-    return recon_loss_x, sam_loss, recon_loss_y, GL
+class CNN(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(CNN, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.e_conv_1 = nn.Conv2d(in_channels, in_channels*2, 
+                                  kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.e_bn_1 = nn.BatchNorm2d(in_channels*2)
+        self.e_conv_2 = nn.Conv2d(in_channels*2, in_channels*2, 
+                                  kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.e_bn_2 = nn.BatchNorm2d(in_channels*2)
+        self.e_conv_3 = nn.Conv2d(in_channels*2, out_channels, 
+                                  kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.e_bn_3 = nn.BatchNorm2d(out_channels)
+        
+
+    def forward(self, x):
+        #h1
+        x = self.e_conv_1(x)
+        x = F.leaky_relu(x, negative_slope=0.2, inplace=True)
+        x = self.e_bn_1(x)
+        #h2
+        x = self.e_conv_2(x)
+        x = F.leaky_relu(x, negative_slope=0.2, inplace=True)    
+        x = self.e_bn_2(x)     
+        #h3
+        x = self.e_conv_3(x)
+        x = F.leaky_relu(x, negative_slope=0.2, inplace=True) 
+        x = self.e_bn_3(x)
+        return x
+
+
+class AutoEncoder2(nn.Module):
+    def __init__(self, y_channel, z_channel):
+        super(AutoEncoder2, self).__init__()
+        self.y_channel = y_channel
+        self.z_channel = z_channel
+        self.decoder = Decoder(y_channel)
+        self.cnn = CNN(y_channel+z_channel, y_channel)
+        # Reinitialize weights using He initialization
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.detach())
+                m.bias.detach().zero_()
+            elif isinstance(m, torch.nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight.detach())
+                m.bias.detach().zero_()
+            elif isinstance(m, torch.nn.Linear):
+                nn.init.kaiming_normal_(m.weight.detach())
+                m.bias.detach().zero_()
+
+    def forward(self, y, z):
+        x_h = self.decoder(y)
+        x_h_opt = self.cnn(torch.cat([x_h, z], 1))
+        return x_h, x_h_opt
+
+    def calc_loss(self, x1, x2, lz, x):
+        loss = nn.MSELoss()
+        recon_loss_x1 = loss(x1, x)
+        recon_loss_x2 = loss(x2, x)
+        # SAM loss
+        sam_loss = (calc_sam_loss(x2, x) + calc_sam_loss(x1, x))/2
+        ### Graph Laplacian Loss
+        x2 = x2.reshape(x2.shape[0], x2.shape[1], -1)
+        lz_xt = linear_operator.utils.sparse.bdsmm(lz, x2.transpose(1,2))
+        GL = MatmulLinearOperator(x2, lz_xt)
+        factor = x2.shape[-1]
+        GL = torch.diagonal(to_dense(GL)/factor, dim1=-2, dim2=-1).sum() # trace
+        return recon_loss_x1, recon_loss_x2, sam_loss, GL
+        
+
+
 
 
 
--- hmi_fusion/models/dbglrf/train_new.py
+++ hmi_fusion/models/dbglrf/train_new.py
@@ -1,4 +1,4 @@
-from .dbglrf import AutoEncoder
+from .dbglrf import AutoEncoder2
 from datasets.cave_dataset import CAVEDataset
 import numpy as np
 from torch.optim import Adam
@@ -11,14 +11,14 @@ import pdb
 ## config
 epochs = 40
 batch_size = 8
-in_channels = 31
-dec_channels = 31
+y_channel = 31
+z_channel = 3
 lr = 1e-3
-model_name = "ae"
+model_name = "ae2"
 load_best_model = False
 model_path = f"./artifacts/{model_name}/gmodel.pth"
-if not os.path.exists("./artifacts/{model_name}"):
-    os.makedirs("./artifacts/{model_name}")
+if not os.path.exists(f"./artifacts/{model_name}"):
+    os.makedirs(f"./artifacts/{model_name}")
 
 writer = SummaryWriter("./artifacts/{model_name}")
 train_dataset = CAVEDataset("./datasets/data/CAVE", None, mode="train")
@@ -27,7 +27,7 @@ train_loader = torch.utils.data.DataLoader(train_dataset,
                                            shuffle=True, batch_size=batch_size)
 test_loader = torch.utils.data.DataLoader(test_dataset,
                                           shuffle=False, batch_size=batch_size)
-model = AutoEncoder(in_channels=in_channels, dec_channels=dec_channels)
+model = AutoEncoder2(y_channel, z_channel)
 if load_best_model:
     model.load_state_dict(torch.load(model_path))
 optimizer = Adam(model.parameters(), lr=lr)
@@ -36,24 +36,25 @@ alpha, beta, gamma, delta = 1, 1, 1.5, 1
 best_model_test_loss = np.inf
 for epoch in range(epochs):
     model.train()
-    train_recon_loss_x = 0
-    train_recon_loss_y = 0
+    train_recon_loss_x1 = 0
+    train_recon_loss_x2 = 0
     train_sam_loss = 0
     train_gl_loss = 0
     total_train_loss = 0
     for items in train_loader:
         optimizer.zero_grad()
         c, y, z, x, lz = items
-        y_hat, x_hat = model(x, y, z, mode="train")
-        recon_loss_x, sam_loss, recon_loss_y, GL = model.calc_loss(x_hat, y_hat, lz, x, y)
-        total_loss = alpha*recon_loss_x + beta*recon_loss_y + gamma*sam_loss + delta*GL 
+        
+        x1, x2 = model(y, z)
+        recon_loss_x1, recon_loss_x2, sam_loss, GL = model.calc_loss(x1, x2, lz, x)
+        total_loss = alpha*recon_loss_x1 + beta*recon_loss_x2 + gamma*sam_loss + delta*GL 
         total_loss.backward()
         optimizer.step()
-        train_recon_loss_x += recon_loss_x.item()
-        train_recon_loss_y += recon_loss_y.item()
+        train_recon_loss_x1 += recon_loss_x1.item()
+        train_recon_loss_x2 += recon_loss_x2.item()
         train_sam_loss += sam_loss.item() 
         train_gl_loss += GL.item()
-        total_train_loss += (recon_loss_x + sam_loss + GL).item()
+        total_train_loss += (recon_loss_x2 + sam_loss + GL).item()
 
     writer.add_scalar("Loss/train", total_train_loss/len(train_loader), epoch)
     writer.add_scalar("Loss_GL/train", train_gl_loss/len(train_loader), epoch)
@@ -61,27 +62,27 @@ for epoch in range(epochs):
     # get eval loss every 2 epochs
     if epoch % 2 == 0:
         print(f"train epoch: {epoch} \
-        recon_x loss: {train_recon_loss_x/len(train_loader)}, \
-        recon_y loss: {train_recon_loss_y/len(train_loader)}, \
+        recon_x1 loss: {train_recon_loss_x1/len(train_loader)}, \
+        recon_x2 loss: {train_recon_loss_x2/len(train_loader)}, \
         sam_loss loss: {train_sam_loss/len(train_loader)}, \
         GLaplacian loss: {train_gl_loss/len(train_loader)}")
         model.eval()
-        test_recon_loss_x = 0
+        test_recon_loss_x2 = 0
         test_sam_loss = 0
         test_gl_loss = 0
         total_test_loss = 0
         for items in test_loader:
             c, y, z, x, lz = items
-            y_hat, x_hat = model(x, y, z, mode="test")
-            recon_loss_x, sam_loss, recon_loss_y, GL = model.calc_loss(x_hat, y_hat, lz, x, y)
-            total_loss = recon_loss_x + sam_loss + GL 
-            test_recon_loss_x += recon_loss_x.item()
+            x1, x2 = model(y, z)
+            recon_loss_x1, recon_loss_x2, sam_loss, GL = model.calc_loss(x1, x2, lz, x)
+            total_loss = recon_loss_x2 + sam_loss + GL 
+            test_recon_loss_x2 += recon_loss_x2.item()
             test_sam_loss += sam_loss.item() 
             test_gl_loss += GL.item()
             total_test_loss = total_loss.item()
 
         print(f"test epoch: {epoch} \
-        recon_x loss: {test_recon_loss_x/len(test_loader)}, \
+        recon_x2 loss: {test_recon_loss_x2/len(test_loader)}, \
         sam_loss loss: {test_sam_loss/len(test_loader)}, \
         GLaplacian loss: {test_gl_loss/len(test_loader)}")
         total_test_loss /= len(test_loader)
