a0f6cd1d7cc46d75bc627cc7de53cb5ff10c2357
shubham0704 <sb42451112@gmail.com>
2022-11-23 01:57:24 +0000

    working sparse batchwise LMC for R learning

--- hmi_fusion/models/dbglrf/dbglrf.py
+++ hmi_fusion/models/dbglrf/dbglrf.py
@@ -844,26 +844,50 @@ class MultitaskGPModel(gpytorch.models.ExactGP):
         return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
 
 
-class MultitaskVariationalGPModel(gpytorch.models.ExactGP):
-    def __init__(self, inducing_points):
-
-        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(inducing_points.size(-1))
-        variational_strategy = gpytorch.variational.VariationalStrategy(
-            self, inducing_points, variational_distribution, learn_inducing_locations=True
-        )
-        super().__init__(variational_strategy)
-
-        self.mean_module = gpytorch.means.MultitaskMean(
-            gpytorch.means.ConstantMean(batch_shape=torch.Size([100])), num_tasks=31
-        )
-        self.covar_module = gpytorch.kernels.MultitaskKernel(
-            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([100])), num_tasks=31, rank=1
+class MultitaskVariationalGPModel(gpytorch.models.ApproximateGP):
+    """
+    We are using Linear Model of Co-regionalization
+    This is important when we want output dimensions to have linear dependencies with each
+    other. In our case, all our 31 dimension outputs are not entirely independent.
+    All outputs are dependent on the nearby outputs. Granted that the degree of influence
+    decreases as distance increases. The scaling aspect of the covariance matrix guarantees
+    that to us.
+    """
+    def __init__(self, num_latents=20, num_tasks=31, num_inducing_points=18, input_dim=3):
+        # Let's use a different set of inducing points for each latent function
+        inducing_points = torch.rand(num_latents, num_inducing_points, input_dim)
+        
+        # We have to mark the CholeskyVariationalDistribution as batch
+        # so that we learn a variational distribution for each task
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
+            inducing_points.size(-2), batch_shape=torch.Size([num_latents])
         )
         
-
+        # We have to wrap the VariationalStrategy in a LMCVariationalStrategy
+        # so that the output will be a MultitaskMultivariateNormal rather than a batch output
+        variational_strategy = gpytorch.variational.LMCVariationalStrategy(
+            gpytorch.variational.VariationalStrategy(
+                self, inducing_points, variational_distribution, learn_inducing_locations=True
+            ),
+            num_tasks=num_tasks,
+            num_latents=num_latents,
+            latent_dim=-1
+        )
+        # Each task is a weighted sum of the underlying latents incase of LMC
+        super().__init__(variational_strategy)
+        
+        # The mean and covariance modules should be marked as batch
+        # so we learn a different set of hyperparameters
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([num_latents]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_latents])),
+            batch_shape=torch.Size([num_latents])
+        )
+        
     def forward(self, x):
-        pdb.set_trace()
+        # The forward function should be written as if we were dealing with each output
+        # dimension in batch
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
